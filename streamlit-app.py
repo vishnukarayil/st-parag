1462a09cc11e6de789dae01bfdf63af00a7d375b
vishnukarayil <130930597+vishnukarayil@users.noreply.github.com>
2023-04-21 00:33:38 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -35,5 +35,7 @@ else:
     st.markdown("## [ Your result appears here ]")
     st.markdown("### Disclaimer")
     st.markdown("PARAG is an educational tool that uses the GPT-3.5 language model developed by OpenAI to provide users with information related to pathology. Since this is an alpha release, the language model is still learning, and the responses can be edgy at times. We always recommend the use of standard literatures for more accurate answers. Use at your own risk.  The app name and all it's components are owned by the developer. The current version is free to use.")
-    st.markdown("***Proudly presented by Dr. Vishnu K R***")
+
     
+# Add the static footer
+st.footer("Copyright © 2023, ***Proudly presented by Dr. Vishnu K R***")
