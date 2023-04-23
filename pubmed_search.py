183c1b64bf1f79cbf006336064e2d016d8c823ac
vishnukarayil <130930597+vishnukarayil@users.noreply.github.com>
2023-04-23 23:08:14 +0530

    Update pubmed_search.py

--- pubmed_search.py
+++ pubmed_search.py
@@ -1,19 +1,30 @@
-import pymed
+from pyentrez import entrez
+import urllib.parse
 
 def search_pmc_articles(query):
-    # Initialize the PubMed object
-    p = pymed.PubMed()
+    # Initialize the Entrez object
+    e = entrez()
+
+    # Encode the query for use in the Entrez search API
+    encoded_query = urllib.parse.quote(query)
 
     # Search for articles with the given query in their titles
-    results = p.query(query, reldate=365, page_size=50)
+    results = e.esearch(db='pmc', term=encoded_query, reldate=365)
 
-    # Create a list of dictionaries with article titles and links
+    # Fetch the full records for the search results
+    records = e.efetch(db='pmc', id=results.ids, rettype='medline', retmode='text')
+
+    # Parse the records and extract the article titles and links
     articles = []
-    for result in results:
-        if query.lower() in result.title.lower():
-            articles.append({
-                'title': result.title,
-                'link': f"https://www.ncbi.nlm.nih.gov/pmc/articles/{result.pmc}"
-            })
+    for record in records:
+        title = None
+        link = None
+        for line in record.split('\n'):
+            if line.startswith('TI  - '):
+                title = line[6:]
+            elif line.startswith('PMCID- '):
+                link = f"https://www.ncbi.nlm.nih.gov/pmc/articles/{line[7:]}"
+        if title is not None and link is not None:
+            articles.append({'title': title, 'link': link})
 
     return articles
