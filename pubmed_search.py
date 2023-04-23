de094db131f821d4c81fac94dcae5e4fa0bddd79
vishnukarayil <130930597+vishnukarayil@users.noreply.github.com>
2023-04-24 00:28:04 +0530

    Update pubmed_search.py

--- pubmed_search.py
+++ pubmed_search.py
@@ -1,10 +1,9 @@
 import requests
 import xml.etree.ElementTree as ET
 
-def search_pubmed(query, databases=['pmc', 'pubmed']):
+def search_pubmed(query):
     base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
-    db_param = ",".join(databases)
-    search_url = base_url + f"esearch.fcgi?db={db_param}&retmax=10&term=" + query
+    search_url = base_url + "esearch.fcgi?db=pmc&retmax=10&term=" + query
     search_result = requests.get(search_url)
     root = ET.fromstring(search_result.content)
     id_list = []
@@ -12,13 +11,14 @@ def search_pubmed(query, databases=['pmc', 'pubmed']):
         id_list.append(id.text)
     results = []
     for id in id_list:
-        for db in databases:
-            summary_url = base_url + f"esummary.fcgi?db={db}&id=" + id
-            summary_result = requests.get(summary_url)
+        summary_url = base_url + "esummary.fcgi?db=pmc&id=" + id
+        summary_result = requests.get(summary_url)
+        try:
             root = ET.fromstring(summary_result.content)
-            title = root.find(f"./DocSum/Item[@Name='Title']").text
-            link = f"https://www.ncbi.nlm.nih.gov/{db}/{id}"
+            title = root.find("./DocSum/Item[@Name='Title']").text
+            link = "https://www.ncbi.nlm.nih.gov/pmc/articles/" + id
             if query.lower() in title.lower():
                 results.append({'title': title, 'link': link})
-                break
+        except ET.ParseError as e:
+            print(f"Error parsing XML for article ID {id}: {e}")
     return results
