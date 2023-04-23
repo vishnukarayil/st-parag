a373801361fb12d371d95ccf75be1fca12b9aeb8
vishnukarayil <130930597+vishnukarayil@users.noreply.github.com>
2023-04-23 22:54:53 +0530

    Update pubmed_search.py

--- pubmed_search.py
+++ pubmed_search.py
@@ -1,19 +1,47 @@
-import pymed
+import requests
+from urllib.parse import urlencode
 
-def search_pmc_articles(query):
-    # Initialize the PubMed object
-    p = pymed.PubMed()
+def pubmed_search(query, retmax=10):
+    """
+    Search for articles in PubMed Central using a query term.
 
-    # Search for articles with the given query in their titles
-    results = p.query(query, db='pmc', reldate=365)
+    Parameters:
+    - query (str): The search term.
+    - retmax (int): The maximum number of articles to return (default: 10).
 
-    # Create a list of dictionaries with article titles and links
-    articles = []
-    for result in results:
-        if query.lower() in result.title.lower():
-            articles.append({
-                'title': result.title,
-                'link': f"https://www.ncbi.nlm.nih.gov/pmc/articles/{result.pmc}"
-            })
+    Returns:
+    - article_links (str): A Markdown-formatted string containing links to the matching articles.
+    """
+    base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+    search_url = base_url + "esearch.fcgi"
 
-    return articles
+    # Build the query parameters
+    params = {
+        "db": "pmc",
+        "term": query,
+        "retmax": retmax,
+        "retmode": "json"
+    }
+
+    # Make the API request
+    response = requests.get(search_url + "?" + urlencode(params))
+
+    # Parse the response JSON
+    results = response.json()["esearchresult"]
+
+    # Check for errors
+    if "errorlist" in results:
+        error = results["errorlist"]["error"][0]["message"]
+        return f"Error: {error}"
+
+    # Extract the PMIDs
+    pmids = results["idlist"]
+
+    # Build the article links
+    article_links = ""
+    for pmid in pmids:
+        link = f"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC{pmid}/"
+        title = f"PMC{pmid}"
+        article_links += f"- [{title}]({link})\n"
+
+    return article_links
